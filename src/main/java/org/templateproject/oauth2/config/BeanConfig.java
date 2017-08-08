398eaa17eae5c37d3d1b5122264ecaa007734d5c
765934806@qq.com <wwb547521014>
2017-08-08 20:54:19 +0800

    完善代码

--- src/main/java/org/templateproject/oauth2/config/BeanConfig.java
+++ src/main/java/org/templateproject/oauth2/config/BeanConfig.java
@@ -8,12 +8,13 @@ import org.templateproject.oauth2.support.pojo.SqlMap;
  * created by Wuwenbin on 2017/8/5 at 14:15
  */
 @Configuration
-public class CommonBeanConfig {
+public class BeanConfig {
 
-        @Bean
-        public SqlMap sqlMapper() {
-                SqlMap sqlMap = new SqlMap();
-                sqlMap.setMapperPath("classpath:mappers");
-                return sqlMap;
-        }
+    @Bean
+    public SqlMap sqlMapper() {
+        SqlMap sqlMap = new SqlMap();
+        sqlMap.setMapperPath("classpath:mappers");
+        return sqlMap;
+    }
+
 }
--- src/main/java/org/templateproject/oauth2/config/ListenerConfig.java
+++ src/main/java/org/templateproject/oauth2/config/ListenerConfig.java
@@ -18,4 +18,5 @@ public class ListenerConfig {
         servletListenerRegistrationBean.setListener(new RequestContextListener());
         return servletListenerRegistrationBean;
     }
+
 }
