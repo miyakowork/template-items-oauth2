3ae11f9b1ccf2b4ec1b2b5c2b2c4e5a891139736
765934806@qq.com <wwb547521014>
2017-08-08 15:07:01 +0800

    缩进由8->4

--- src/main/java/org/templateproject/oauth2/Oauth2Application.java
+++ src/main/java/org/templateproject/oauth2/Oauth2Application.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 public class Oauth2Application {
 
-        public static void main(String[] args) {
-                SpringApplication.run(Oauth2Application.class, args);
-        }
+    public static void main(String[] args) {
+        SpringApplication.run(Oauth2Application.class, args);
+    }
 }
