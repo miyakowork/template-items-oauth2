ce0044690021ecee7a3a958adb5a7cf46608b1c2
miyakowork <765934806@qq.com>
2017-08-09 19:09:06 +0800

    增加系统模块下拉框内容获取方式

--- src/main/java/org/templateproject/oauth2/page/SupportRestController.java
+++ src/main/java/org/templateproject/oauth2/page/SupportRestController.java
@@ -1,8 +1,10 @@
 package org.templateproject.oauth2.page;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.templateproject.oauth2.support.BaseRestController;
+import org.templateproject.oauth2.entity.OauthSystemModule;
+import org.templateproject.oauth2.service.SystemModuleService;
 import org.templateproject.oauth2.support.TemplateController;
 import org.templateproject.oauth2.support.pojo.bo.SelectBO;
 
@@ -14,8 +16,15 @@ import java.util.Map;
  * Created by Wuwenbin on 2017/8/1.
  */
 @RestController
+@RequestMapping("oauth2")
 public class SupportRestController extends TemplateController {
 
+    private SystemModuleService systemModuleService;
+
+    @Autowired
+    public void setSystemModuleService(SystemModuleService systemModuleService) {
+        this.systemModuleService = systemModuleService;
+    }
     /**
      * 公共的下拉框搜索对象的值
      *
@@ -28,6 +37,22 @@ public class SupportRestController extends TemplateController {
         selectBOs.add(selectBO1);
         SelectBO selectBO2 = SelectBO.create("0", "不可用");
         selectBOs.add(selectBO2);
+        return searchSelect(selectBOs);
+    }
+
+    /**
+     * 表格中使用系统模块查询的下拉框操作
+     *
+     * @return 下拉框对象
+     */
+    @RequestMapping("/systemModule/select")
+    public Map<Object, Object> systemModuleSelect() {
+        List<SelectBO> selectBOs = new ArrayList<>();
+        List<OauthSystemModule> systemModules = systemModuleService.findAllEnabledSystemModules();
+        for (OauthSystemModule systemModule : systemModules) {
+            SelectBO selectBO = SelectBO.create(systemModule.getSystemCode(), systemModule.getName());
+            selectBOs.add(selectBO);
+        }
         return searchSelect(selectBOs);
     }
 }
