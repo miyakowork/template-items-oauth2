f42e1bd1874d3832b5eb8cc2c2a6b3e1f8c00c7f
miyakowork <765934806@qq.com>
2017-08-09 18:42:58 +0800

    下拉框方法改至到SupportRestController中

--- src/main/java/org/templateproject/oauth2/page/menuModule/MenuModuleRestController.java
+++ src/main/java/org/templateproject/oauth2/page/menuModule/MenuModuleRestController.java
@@ -4,20 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.templateproject.oauth2.entity.OauthMenuModule;
-import org.templateproject.oauth2.entity.OauthSystemModule;
 import org.templateproject.oauth2.service.MenuModuleService;
-import org.templateproject.oauth2.service.SystemModuleService;
 import org.templateproject.oauth2.support.BaseRestController;
 import org.templateproject.oauth2.support.pojo.BootstrapTable;
 import org.templateproject.oauth2.support.pojo.bo.MenuModuleBO;
-import org.templateproject.oauth2.support.pojo.bo.SelectBO;
 import org.templateproject.oauth2.support.pojo.vo.MenuModuleVO;
 import org.templateproject.pojo.page.Page;
 import org.templateproject.pojo.response.R;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 
 /**
  * Created by Wuwenbin on 2017/08/01.
@@ -27,17 +20,13 @@ import java.util.Map;
 public class MenuModuleRestController extends BaseRestController {
 
     private MenuModuleService menuModuleService;
-    private SystemModuleService systemModuleService;
+
 
     @Autowired
     public void setMenuModuleService(MenuModuleService menuModuleService) {
         this.menuModuleService = menuModuleService;
     }
 
-    @Autowired
-    public void setSystemModuleService(SystemModuleService systemModuleService) {
-        this.systemModuleService = systemModuleService;
-    }
 
     /**
      * 菜单模块页面
@@ -108,19 +97,4 @@ public class MenuModuleRestController extends BaseRestController {
         }
     }
 
-    /**
-     * 表格中使用系统模块查询的下拉框操作
-     *
-     * @return 下拉框对象
-     */
-    @RequestMapping("systemModule/select")
-    public Map<Object, Object> systemModuleSelect() {
-        List<SelectBO> selectBOs = new ArrayList<>();
-        List<OauthSystemModule> systemModules = systemModuleService.findAllEnabledSystemModules();
-        for (OauthSystemModule systemModule : systemModules) {
-            SelectBO selectBO = SelectBO.create(systemModule.getSystemCode(), systemModule.getName());
-            selectBOs.add(selectBO);
-        }
-        return searchSelect(selectBOs);
-    }
 }
