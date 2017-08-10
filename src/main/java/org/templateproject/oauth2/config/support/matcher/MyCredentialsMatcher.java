6640b56b6e3c7dbdd2c221419209c6e4ee2c944b
765934806@qq.com <wwb547521014>
2017-08-11 00:09:52 +0800

    删除私有方法获取ip地址，改用HttpUtils工具类来获取

--- src/main/java/org/templateproject/oauth2/config/support/matcher/MyCredentialsMatcher.java
+++ src/main/java/org/templateproject/oauth2/config/support/matcher/MyCredentialsMatcher.java
@@ -17,6 +17,7 @@ import org.templateproject.oauth2.constant.ShiroConsts;
 import org.templateproject.oauth2.entity.OauthUser;
 import org.templateproject.oauth2.service.LogService;
 import org.templateproject.oauth2.service.shiro.ShiroUserService;
+import org.templateproject.oauth2.util.HttpUtils;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -75,7 +76,7 @@ public class MyCredentialsMatcher extends SimpleCredentialsMatcher {
             HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
             request.getSession().setAttribute(ShiroConsts.SESSION_USERNAME_KEY, username);
             try {
-                logService.traceLog(userService.findByUserName(username).getId(), getRemoteAddr(request));
+                logService.traceLog(userService.findByUserName(username).getId(), HttpUtils.getRemoteAddr(request));
             } catch (Exception e) {
                 LOGGER.error("记录用户登录日志失败，异常信息：{}", e);
             }
@@ -102,35 +103,4 @@ public class MyCredentialsMatcher extends SimpleCredentialsMatcher {
     }
 
 
-    /**
-     * 获取IP地址
-     * <p>
-     * 使用Nginx等反向代理软件， 则不能通过request.getRemoteAddr()获取IP地址
-     * 如果使用了多级反向代理的话，X-Forwarded-For的值并不止一个，而是一串IP地址，X-Forwarded-For中第一个非unknown的有效IP字符串，则为真实IP地址
-     *
-     * @return
-     */
-    private String getRemoteAddr(HttpServletRequest request) {
-        String remoteAddress = null;
-        try {
-            remoteAddress = request.getHeader("x-forwarded-for");
-            if (StringUtils.isEmpty(remoteAddress) || "unknown".equalsIgnoreCase(remoteAddress)) {
-                remoteAddress = request.getHeader("Proxy-Client-IP");
-            }
-            if (StringUtils.isEmpty(remoteAddress) || remoteAddress.length() == 0 || "unknown".equalsIgnoreCase(remoteAddress)) {
-                remoteAddress = request.getHeader("WL-Proxy-Client-IP");
-            }
-            if (StringUtils.isEmpty(remoteAddress) || "unknown".equalsIgnoreCase(remoteAddress)) {
-                remoteAddress = request.getHeader("HTTP_CLIENT_IP");
-            }
-            if (StringUtils.isEmpty(remoteAddress) || "unknown".equalsIgnoreCase(remoteAddress)) {
-                remoteAddress = request.getHeader("HTTP_X_FORWARDED_FOR");
-            }
-            if (StringUtils.isEmpty(remoteAddress) || "unknown".equalsIgnoreCase(remoteAddress)) {
-                remoteAddress = request.getRemoteAddr();
-            }
-        } catch (Exception ignored) {
-        }
-        return remoteAddress;
-    }
 }
