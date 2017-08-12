5769552af4bb39186357e8865f57e730d0935a1e
765934806@qq.com <wwb547521014>
2017-08-12 16:01:32 +0800

    system_module代码格式优化

--- src/main/java/org/templateproject/oauth2/service/SystemModuleService.java
+++ src/main/java/org/templateproject/oauth2/service/SystemModuleService.java
@@ -22,63 +22,64 @@ import java.util.List;
 @SqlMapper("system_module")
 public class SystemModuleService extends SimpleBaseCrudService<OauthSystemModule, Integer> {
 
-        /**
-         * 查询systemModule当前页面数据
-         *
-         * @param page           page
-         * @param systemModuleBo 查询控件参数对象
-         * @return page
-         */
-        public Page<SystemModuleVO> findSystemModulePage( Page<SystemModuleVO> page, SystemModuleBo systemModuleBo) {
-                return findPagination(page, SystemModuleVO.class,sql(), systemModuleBo);
-        }
+    /**
+     * 查询systemModule当前页面数据
+     *
+     * @param page           page
+     * @param systemModuleBo 查询控件参数对象
+     * @return page
+     */
+    public Page<SystemModuleVO> findSystemModulePage(Page<SystemModuleVO> page, SystemModuleBo systemModuleBo) {
+        return findPagination(page, SystemModuleVO.class, sql(), systemModuleBo);
+    }
 
-        /**
-         * 表面上是删除其实是更新一个enabled字段为0
-         *
-         * @param ids ids
-         * @throws Exception e
-         */
-        public void deleteModules(String[] ids) throws Exception {
-                executeBatch(sql(), "id", ids);
-        }
+    /**
+     * 表面上是删除其实是更新一个enabled字段为0
+     *
+     * @param ids ids
+     * @throws Exception e
+     */
+    public void deleteModules(String[] ids) throws Exception {
+        executeBatch(sql(), "id", ids);
+    }
 
-        /**
-         * @return 查找所有可用的系统模块
-         */
-        public List<OauthSystemModule> findAllEnabledSystemModules() {
-                return h2Dao.findListBeanByArray(sql(), OauthSystemModule.class);
-        }
+    /**
+     * @return 查找所有可用的系统模块
+     */
+    public List<OauthSystemModule> findAllEnabledSystemModules() {
+        return h2Dao.findListBeanByArray(sql(), OauthSystemModule.class);
+    }
 
 
-        /**
-         * module转zTree
-         *
-         * @param systemModules systemModules
-         * @return List<ZTreeBO>
-         */
-        public List<ZTreeBO<String>> module2ZTree(List<OauthSystemModule> systemModules) {
-                List<ZTreeBO<String>> zTreeBOList = new ArrayList<>(systemModules.size());
-                for (OauthSystemModule oauthSystemModule : systemModules) {
-                        ZTreeBO<String> zTreeBO = new ZTreeBO<>();
-                        zTreeBO.setId(oauthSystemModule.getId().toString());
-                        zTreeBO.setpId("0");
-                        zTreeBO.setName(oauthSystemModule.getName());
-                        zTreeBO.setOther(oauthSystemModule.getSystemCode());
-                        zTreeBOList.add(zTreeBO);
-                }
-                return zTreeBOList;
+    /**
+     * module转zTree
+     *
+     * @param systemModules systemModules
+     * @return List<ZTreeBO>
+     */
+    public List<ZTreeBO<String>> module2ZTree(List<OauthSystemModule> systemModules) {
+        List<ZTreeBO<String>> zTreeBOList = new ArrayList<>(systemModules.size());
+        for (OauthSystemModule oauthSystemModule : systemModules) {
+            ZTreeBO<String> zTreeBO = new ZTreeBO<>();
+            zTreeBO.setId(oauthSystemModule.getId().toString());
+            zTreeBO.setpId("0");
+            zTreeBO.setName(oauthSystemModule.getName());
+            zTreeBO.setOther(oauthSystemModule.getSystemCode());
+            zTreeBO.setisParent(true);
+            zTreeBOList.add(zTreeBO);
         }
+        return zTreeBOList;
+    }
 
-        /**
-         * 查询是否已经有存在的system code
-         *
-         * @param systemCode 系统代码
-         * @return 是否存在
-         */
-        public boolean isExistSystemCode( String systemCode) {
-                long cnt = h2Dao.queryNumberByArray(sql(), Long.class, systemCode);
-                return cnt > 0;
-        }
+    /**
+     * 查询是否已经有存在的system code
+     *
+     * @param systemCode 系统代码
+     * @return 是否存在
+     */
+    public boolean isExistSystemCode(String systemCode) {
+        long cnt = h2Dao.queryNumberByArray(sql(), Long.class, systemCode);
+        return cnt > 0;
+    }
 
 }
